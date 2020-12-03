2485a673e1b73519ebafddf5c263c42a518f160b
Muneeb H. Jan <muneeb_hafeez.jan@mailbox.tu-dresden.de>
2020-12-03 14:09:29 +0100

    Made changes to grproxy to redirect to specific URLs and compiled.

--- grproxy/src/grproxy/grproxy.go
+++ grproxy/src/grproxy/grproxy.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"math/rand"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 )
 
+// NewMultipleHostReverseProxy creates a reverse proxy that will randomly select a host from the passed param `targets` if the url request contains /library
+// Targets are explicitly defined in main.
+func NewMultipleHostReverseProxy(targets []*url.URL) *httputil.ReverseProxy {
+	director := func(req *http.Request) {
+		if req.URL.Path == "/library" {
+			fmt.Println("This should redirect to gserve servers after catching library in round robin.")
+			target := targets[rand.Int()%len(targets)]
+			req.URL.Scheme = target.Scheme
+			req.URL.Host = target.Host
+			req.URL.Path = target.Path
+		} else {
+			fmt.Println("This should redirect to nginx for all other cases.")
+			req.URL.Scheme = "http"
+			req.URL.Host = "nginx"
+		}
+	}
+	return &httputil.ReverseProxy{Director: director}
+}
+
 func main() {
-	proxy := httputil.NewSingleHostReverseProxy(&url.URL{
-		Scheme: "http",
-		Host:   "nginx",
+	proxy := NewMultipleHostReverseProxy([]*url.URL{
+		{
+			Scheme: "http",
+			Host:   "gserve1",
+		},
+		{
+			Scheme: "http",
+			Host:   "gserve2",
+		},
 	})
-	http.ListenAndServe(":8080", proxy)
+	log.Fatal(http.ListenAndServe(":8080", proxy))
 }
