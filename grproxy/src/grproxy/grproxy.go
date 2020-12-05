735f41cb20492070a4a47e95e95c1bd59fe873e3
Muneeb H. Jan <muneeb_hafeez.jan@mailbox.tu-dresden.de>
2020-12-05 18:10:44 +0100

    Added connection variable with Zookeeper to grproxy and gserve. Updated version in hbase Docker file and rectified docker-compose file (Zookeeper misspelled).

--- grproxy/src/grproxy/grproxy.go
+++ grproxy/src/grproxy/grproxy.go
@@ -7,7 +7,19 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"time"
+
+	"github.com/samuel/go-zookeeper/zk"
 )
+
+// Function to connect with Zookeeper
+func connect() *zk.Conn {
+	conn, _, err := zk.Connect([]string{"zookeeper:2181"}, time.Second)
+	if err != nil {
+		panic(err)
+	}
+	return conn
+}
 
 // NewMultipleHostReverseProxy creates a reverse proxy that will randomly select a host from the passed param `targets` if the url request contains /library
 // Targets are explicitly defined in main.
@@ -28,6 +40,15 @@ func NewMultipleHostReverseProxy(targets []*url.URL) *httputil.ReverseProxy {
 }
 
 func main() {
+
+	//Create connection to zookeeper and holding function.
+	conn := connect()
+	defer conn.Close()
+
+	for conn.State() != zk.StateHasSession {
+		time.Sleep(5)
+	}
+
 	proxy := NewMultipleHostReverseProxy([]*url.URL{
 		{
 			Scheme: "http",
--- gserve/src/gserve/gserve.go
+++ gserve/src/gserve/gserve.go
@@ -4,9 +4,21 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
+
+	"github.com/samuel/go-zookeeper/zk"
 )
 
 var my_name string = "Undecided."
+
+// Function to connect with Zookeeper
+func connect() *zk.Conn {
+	conn, _, err := zk.Connect([]string{"zookeeper:2181"}, time.Second)
+	if err != nil {
+		panic(err)
+	}
+	return conn
+}
 
 func library(w http.ResponseWriter, req *http.Request) {
 
@@ -14,8 +26,19 @@ func library(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-
+	// Get environment gsname. Defined in docker-compose in our case.
 	my_name = os.Getenv("gsname")
+
+	// Connect to Zookeeper and hold any function till session is built.
+	conn := connect()
+	defer conn.Close()
+
+	for conn.State() != zk.StateHasSession {
+		time.Sleep(5)
+	}
+	if conn.State() == zk.StateHasSession {
+		fmt.Printf(" %s has successfully connected to Zookeeper\n", my_name)
+	}
 
 	http.HandleFunc("/library", library)
 
