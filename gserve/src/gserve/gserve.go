8adafe6ebe912db4a6586afe7297c313e099de21
Muneeb H. Jan <muneeb_hafeez.jan@mailbox.tu-dresden.de>
2020-12-05 13:17:31 +0100

    Added environment variable to docker compose and displays name in gserve.

--- gserve/src/gserve/gserve.go
+++ gserve/src/gserve/gserve.go
@@ -1,18 +1,23 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
+	"os"
 )
+
+var my_name string = "Undecided."
 
 func library(w http.ResponseWriter, req *http.Request) {
 
-    fmt.Fprintf(w, "Welcome to Gserve!. Have a nice day.\n")
+	fmt.Fprintf(w, "Proudly served by %s\n", my_name)
 }
 
 func main() {
 
-    http.HandleFunc("/library", library)
+	my_name = os.Getenv("gsname")
 
-    http.ListenAndServe(":80", nil)
+	http.HandleFunc("/library", library)
+
+	http.ListenAndServe(":80", nil)
 }
